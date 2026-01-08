94530289079a5f5553e7e871e95e191b7cbbebea
VAN <937058311@qq.com>
2026-01-08 16:20:50 +0800

    初始化部分信息，屏蔽评论区内容

--- src/consts.ts
+++ src/consts.ts
@@ -1,7 +1,7 @@
 // 将所有全局数据放入此文件中。
 // 您可以使用 `import` 关键字在站点的任何位置导入此数据。
 
-export const SITE_TITLE = '子舒的博客';
-export const SITE_DESCRIPTION = '想留下点什么，证明我来过。';
-export const SITE_AUTHOR = '子舒';
-export const SITE_DAYS = '2020-06-14';
+export const SITE_TITLE = '阿水的博客';
+export const SITE_DESCRIPTION = '记录生活，分享思考。';
+export const SITE_AUTHOR = '阿水';
+export const SITE_DAYS = '2026-01-08';
